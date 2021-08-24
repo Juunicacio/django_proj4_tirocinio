4943bdbef603b443d2163603945eaead51336129
Juunicacio <juhnicacio.bio@gmail.com>
2021-08-24 10:13:53 +0200

    with the if statement inside manage.py the commands migrate and makemigrations were not working, put the manage.py file as the original setup and now we need to run with daphne to see if it will works

--- satellite_telemetry_data_analysis-project/manage.py
+++ satellite_telemetry_data_analysis-project/manage.py
@@ -5,9 +5,10 @@ import sys
 import django
 from data_analysis.scripts.data_analysis.main import *
 
+# run in local
+#def main():
 
-def main():
-    #run_main_of_data_analysis()
+if __name__ == '__main__':
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'satellite_telemetry_data_analysis.settings')
     try:
@@ -20,26 +21,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
-#def run_main_of_data_analysis():
-    #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.setting')
-    #django.setup()
-    # now your code can go here...
-    # Call here the function data_analysis_main() inside main.py of data_analysis app
-    #data_analysis_main()
-
-# run on server
-if __name__ == '__main__':
-    if __name__ == 'python3 manage.py runserver' or __name__ == 'py manage.py runserver':
-        data_analysis_main()
-        print('')
-        print("Now, you can run: 'daphne satellite_telemetry_data_analysis.asgi:application' to see the website")
-
-    if __name__ == 'daphne satellite_telemetry_data_analysis.asgi:application':
-        """
-        Before running the application, make sure you had ran the script for data analysis before
-        If you had already done that, please ignore this message, and go open your localhost
-        """
-        main()
 # run in local
 # if __name__ == '__main__':
 #     if __name__ == 'daphne satellite_telemetry_data_analysis.asgi:application':
